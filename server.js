36e1cd10d8499d8e13e5ecdc77672080a0ff09ec
Tom Johnson <trjohnson3@gmail.com>
2024-06-22 07:27:24 -0500

    initial route set up

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: 'true', message: 'show all bootcamps' });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: 'true', message: 'add new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: 'true', message: `show bootcamp id: ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: 'true', message: `delete bootcamp id: ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
